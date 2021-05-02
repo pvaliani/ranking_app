0acebc2cc48d0a772536235fb9b0bb94c70e2206
Pedram Valiani <pedram@Pedrams-MacBook-Pro.local>
2021-05-02 17:54:56 +0100

    updates to ranking and rankingresult components to set up

--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -1,2 +1,15 @@
 import React, { Component } from "react";
 import { Button, Header } from 'semantic-ui-react'
+
+// Ranking component generates vote objects in an array for user voting 
+class Ranking extends Component {
+    render() {
+        return (
+            <>
+            This is a Ranking
+            </>
+        );
+    }
+  }
+  
+  export default Ranking;
--- src/components/RankingResult.js
+++ src/components/RankingResult.js
@@ -1,3 +1,15 @@
 import React, { Component } from "react";
 import AppHeader from "./AppHeader";
 
+class RankingResult extends Component {
+    render() {
+        return (
+            <>
+            This is the Ranking Result page
+            </>
+        );
+     
+  }
+}
+  export default RankingResult;
+  
