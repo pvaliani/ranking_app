db6d4fa5c0eda56eeb041960cb044c4bcffe97ad
Pedram Valiani <pedram@Pedrams-MacBook-Pro.local>
2021-05-02 22:56:04 +0100

    added article object styling

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,37 +1,58 @@
 import React, { Component } from "react";
 
+import { Grid, Divider, Header, Container } from "semantic-ui-react";
+
 // Article component maps the body object from an array to render
 // each model attribute/JSON key/value pair
 
 class Article extends Component {
-// Map the article objects body key to render all objects and data within
-
-  render() {
-    return (
-        <>
-          {this.props.article.body.map((bodyIndex) => {
-            return (
-                <div>
   
-                  {<h1>{bodyIndex.type}</h1>}
+  render() {
+
+    
+
+
+
+    return (
+      <>
+       <Grid>
+            <Grid.Column textAlign="justified">  
+      <Container text>
+      <Header><Divider>{this.props.article.title}</Divider></Header>
+        {this.props.article.body.map((bodyIndex) => {
+          return (
+            <>
+              <div>
                 
-                  <p>{bodyIndex.model.text}</p>
-                    <img
-                      src={bodyIndex.model.url}
-                      alt={bodyIndex.model.altText}
-                      style={{
-                        width: bodyIndex.model.width,
-                        height: bodyIndex.model.height
-                      }}
-                    />
-                  <p>{bodyIndex.model.type}</p>
-                  <p>{bodyIndex.model.items}</p>
-                </div>   
-            );
-          })}
-        </>
-      );
-    }
+                
+            
+                <Container text> {<h1>{bodyIndex.type}</h1>}</Container>
+            
+                
+              
+                <Container text><p>{bodyIndex.model.text}</p></Container>
+                  <img
+                    src={bodyIndex.model.url}
+                    alt={bodyIndex.model.altText}
+                    style={{
+                      width: bodyIndex.model.width,
+                      height: bodyIndex.model.height
+                    }}
+                  />
+                <Container><p>{bodyIndex.model.type}</p></Container>
+                <Container><p>{bodyIndex.model.items}</p></Container>
+             
+              
+              </div>  
+            </> 
+          );
+        })}
+        </Container>
+        </Grid.Column>
+              </Grid>
+      </>
+    );
+  }
 }
 
 export default Article;
