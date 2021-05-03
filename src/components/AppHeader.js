8476b6a59c8a47798066769402fe93e505b286a2
Pedram Valiani <pedram@Pedrams-MacBook-Pro.local>
2021-05-03 17:19:26 +0100

    tidy ups

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -2,30 +2,17 @@ import React, { Component } from 'react'
 import { Grid, Container, Header, Image } from 'semantic-ui-react'
 import BBCLogo from '../assets/BBCLogo.png';
 
-
-
-
-
-
-
-// -------------
-
-
+// AppHeader component represents the BBC logo header
 class AppHeader extends Component {
     
-   
-  
+
     render() {
     
-        
       return (
         <>
        
-    
         <Header className="appHeader" textAlign="center"  as='h1'>
           <Image src={BBCLogo} className='logo' /> 
-       
-         
         </Header> 
      
         </>
